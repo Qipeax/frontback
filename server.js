cf431bd83a663fb5a886f88f1e411aadee00df69
Qipeax <qipeax@gmail.com>
2025-02-25 22:58:55 +0300

    Ну и срань

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Обслуживание статических файлов из папки build
+app.use(express.static(path.join(__dirname, "build")));
+
+// Обработка всех маршрутов (для SPA)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Запуск сервера
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
