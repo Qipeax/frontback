ef8dd12b3e328ebc2b6b80b9b80c4d1fe90be213
Qipeax <qipeax@gmail.com>
2025-02-26 12:47:35 +0300

    урааааааааааа

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
 // Обслуживание статических файлов из папки build
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Обработка всех маршрутов (для SPA)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Запуск сервера
