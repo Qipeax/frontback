721f0ea1c873f2269b3fd5ca3ce39923edcd5e56
Qipeax <qipeax@gmail.com>
2025-02-26 12:50:33 +0300

    Хочу убить себя

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Обслуживание статических файлов из папки build
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'test-app', 'build')));
 
 // Обработка всех маршрутов (для SPA)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'test-app', 'build', 'index.html'));
 });
 
 // Запуск сервера
